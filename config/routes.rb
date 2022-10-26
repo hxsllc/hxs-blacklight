4c2229b1e4700d4b6cc28dd6a403d030043c91e6
James Fawks <jefawks3@gmail.com>
2022-10-26 16:22:20 -0400

    Initializing the Project Blacklight application

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  mount Blacklight::Engine => '/'
+  concern :marc_viewable, Blacklight::Marc::Routes::MarcViewable.new
+  root to: "catalog#index"
+  concern :searchable, Blacklight::Routes::Searchable.new
+
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+  devise_for :users
+
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns [:exportable, :marc_viewable]
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
