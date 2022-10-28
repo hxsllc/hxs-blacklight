bac21a014090c78cf8605f69f84c43a882dcfa5a
James Fawks <jefawks3@gmail.com>
2022-10-28 13:16:46 -0400

    Changing the default file watcher in the development environment to `ActiveSupport::FileUpdateChecker` so that it pulls changes rather than listen to file events. See https://stackoverflow.com/questions/37699573/rails-app-in-docker-container-doesnt-reload-in-development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # FIXES Code changes not being reflected in the Docker dev environment
+  # Using the ActiveSupport::FileUpdateChecker which pulls rather listens to events
+  # See https://stackoverflow.com/questions/37699573/rails-app-in-docker-container-doesnt-reload-in-development
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
