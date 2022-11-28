1427b1e24d0055527e45d2a1d39df4f0ee137703
James Fawks <jefawks3@gmail.com>
2022-11-28 12:11:35 -0500

    Moving the `bundle install` to the entry point script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Install missing gems
+bundle check || bundle install --jobs 20 --retry 5
+
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
