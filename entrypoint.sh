7f583908caa4d567e989693f8f133f91f90c8970
James Fawks <jefawks3@gmail.com>
2023-01-31 14:37:04 -0500

    Making sure the development environment is using the development version of webpack config

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ bundle check || bundle install --jobs 20 --retry 5
 yarn install --network-timeout=30000
 
 # Build JS before start
-yarn build
+yarn build:dev
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
