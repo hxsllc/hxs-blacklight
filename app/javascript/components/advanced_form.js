0a57d64b64713a60fab3e745830cfd826e617be1
Ben Bakelaar <ben@hxsdesign.com>
2023-03-04 10:59:56 -0500

    Tested SDM advanced search code

--- app/javascript/components/advanced_form.js
+++ app/javascript/components/advanced_form.js
@@ -1,3 +1,42 @@
+ /* 
+
+code from SDM 
+
+Invalid query parameters: expected Array (got String) for param `all_fields'
+
+$(document).ready(function() {
+    $("form.advanced").submit(function(e) {
+        $(".advanced-search-field").each(function (idx, container) {
+            if($(container).find("input[type=text]").length == 2) {
+                // range search
+                var start = $(container).find("input.start").val();
+                var end = $(container).find("input.end").val();
+                var input_name = ""
+                var input_value = "";
+                if(start || end) {
+                    if (!start) { start = "*"; }
+                    if (!end) { end = "*"; }
+                    input_name = $(container).find("select").val() + "[]";
+                    input_value = "[" + start + " TO " + end + "]";
+                }
+                $(container).find("input[type=hidden]").attr("name", input_name);
+                $(container).find("input[type=hidden]").attr("value", input_value);
+            } else {
+                // text search
+                var input_element = $(container).find("input").first();
+                var input_name = "";
+                if($(input_element).val()) {
+                    input_name = $(container).find("select").val() + "[]";
+                }
+                $(input_element).attr("name", input_name);
+            }
+        });
+    });
+});
+
+*/
+/* code from HXS/James */
+
 $(document).ready(function() {
     $("form.advanced").submit(function(e) {
         var $form = $(this)
